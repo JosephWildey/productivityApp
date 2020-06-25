81bcbe5d0e9a9977ad0d3218f11257bc0de47b44
Joseph Wildey <joewildey@outlook.com>
2020-06-24 20:25:25 -0400

    Add files via upload
    - Major redesign of GUI
    - Add Entries now triggers pop up
    - No longer reads from file
    
    Known bugs:
    - Doesn't close once the pop up is open

---
+++ Python/schedule.py
@@ -0,0 +1,37 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+events = Gtk.ListStore(str, str, str, str)
+
+class ScheduleApplication:
+# exit application
+    def onDestroy(self, *args):
+        Gtk.main_quit()
+
+# retrieve app window
+builder = Gtk.Builder()
+builder.add_from_file("windowMain.glade")
+
+# retrieve the window object
+window = builder.get_object("wdwMain")
+addEntryWindow = builder.get_object("wdwAdd")
+
+def on_AddButton_clicked(self):
+    addEntryWindow.show_all()
+
+# retrive the addbutton
+addButton = builder.get_object("btnAddPopup")
+addButton.connect("clicked", on_AddButton_clicked)
+
+# retrieve the TreeView Object
+mainDisplay = builder.get_object("trvMain")
+
+# connect treeview to liststore
+mainDisplay.set_model(events)
+
+window.show_all()
+
+Gtk.main()
